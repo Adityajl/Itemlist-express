4cc54f20951fecca2c41d2e25f316a31417f2985
Adityajl <aj7002452@gmail.com>
2020-07-02 16:39:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express();
+
+ let items = ['Get Fresh!', 'Listen Music', 'code and coffee'];
+
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static('public'))
+
+app.get('/', function(req,res){
+	var today = new Date();
+
+	var options = {
+		weekday : 'long',
+        day: 'numeric',
+        month: 'long'
+	};
+
+	var day = today.toLocaleDateString('en-US', options)
+
+	res.render('list', {
+		DayList: day,
+		newListItems: items
+	})
+})
+
+app.post('/', function(req,res){
+	let item = req.body.newItem
+
+	items.push(item)
+	
+	res.redirect('/')
+})
+
+app.listen(5000, function(){
+	console.log('server is running')
+})
